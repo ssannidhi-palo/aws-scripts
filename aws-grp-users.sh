dfbae41a07997b697915c8d414fad3d5bab46229
Swati Sannidhi <ssannidhi@palo-it.com>
2019-09-11 17:36:37 +0800

    Reformatted output, added usage info

--- aws-grp-users.sh
+++ aws-grp-users.sh
@@ -1,10 +1,16 @@
+#Usage: ./aws-grp-users.sh 
+#Description: This script generates a list of IAM groups and the IAM Users in each group
+#Output Fields: GroupName:
+#               Users:
+#Author: Swati Sannidhi 
+
 #!/usr/bin/env bash
 
 grp=$(aws iam list-groups --profile paloit-sg --query 'Groups[*].{GroupName:GroupName}' --output text)
 
 for i in $grp ;
 do
-users=$(aws iam get-group --profile paloit-sg --group-name $i --query 'Users[*].{UserName:UserName}' --output text)
-echo -e "\nGroupName:" $i
-echo -e "\t Users:" $users
+echo -e "GroupName:" $i
+echo -e "Users:"
+aws iam get-group --profile paloit-sg --group-name $i --query 'Users[*].{Users:UserName}' --output table | tail -n +5
 done
